0f772c3ede0abdd1c2c3d7d8feb6399d099ec10e
Dmytro Chyr <dima543439@gmail.com>
2024-08-12 17:16:56 +0300

    Add file for Netify

--- src/components/ShopPage/ShopPage.tsx
+++ src/components/ShopPage/ShopPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import CategorySection from "../CategorySection/CategorySection";
 import LoadingScreen from "../Loader/Loader";
 import categoriesData from "../Sider/Category.json";
