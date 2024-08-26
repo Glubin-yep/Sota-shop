f91f91288985cf192fda03b2eda3c89fd8cf82b0
Denys Markevych <denysjse@gmail.com>
2024-08-26 10:44:36 +0300

    Create adaptation for product page

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -2,19 +2,22 @@ import { ReactNode } from 'react'
 import Header from 'layout/header/Header.tsx'
 import Sidebar from 'components/sidebar/Sidebar.tsx'
 import useWindowSize from 'hooks/useWindowSize.ts'
+import MobileBottomMenu from 'components/mobile-bottom-menu/MobileBottomMenu.tsx'
 
 function Layout({ children }: { children: ReactNode }) {
 	const { width } = useWindowSize()
 
-	const isMobile = width ? width <= 992 : false
+	const isTablet = width ? width <= 992 : false
+	const isMobile = width ? width <= 768 : false
 
 	return (
 		<main>
 			<Header />
 			<div className='flex pt-20 gap-2 max-md:pt-16'>
-				{!isMobile && <Sidebar />}
+				{!isTablet && <Sidebar />}
 				<div className='pl-64 w-full max-md:pl-0'>{children}</div>
 			</div>
+			{isMobile && <MobileBottomMenu />}
 		</main>
 	)
 }
